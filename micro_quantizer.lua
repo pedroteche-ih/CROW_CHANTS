4d6c9f45dcd8bb0d6c7375f44c34a80beee214bd
pedroteche-ih <94225235+pedroteche-ih@users.noreply.github.com>
2022-07-08 22:28:33 -0300

    Added Hexany to Quantizer

--- micro_quantizer.lua
+++ micro_quantizer.lua
@@ -8,9 +8,11 @@
 --INTENSE DIATONIC
 --SCALE = {1/1, 20/19, 20/17, 4/3, 3/2, 30/19, 30/17}
 --PROMETHEAN
-SCALE = {1/1, 9/8, 5/4, 7/5, 5/3, 7/4}
+--SCALE = {1/1, 9/8, 5/4, 7/5, 5/3, 7/4}
+-- Hexany 1-3-5-7 I
+SCALE = {1/1, 7/6, 5/4, 35/24, 5/3, 7/4}
 function init()
-    input[2].mode('change', 1, 0.3, 'rising')
+    input[2].mode('change', 0.5, 0.1, 'rising')
     output[1].scale(SCALE, 'ji')
 end
 
@@ -21,8 +23,6 @@ end
 function delay_grab()
 	-- Delays grabbing the 1v/Oct
 	local m_volts = input[1].volts
-	output[1].volts = m_volts+1
-	output[2](pulse(0.1, 5))
-    output[3].volts = m_volts+1
-	output[4](pulse(0.1, 5))
+	output[1].volts = m_volts+2
+	output[2](pulse(0.01, 5))
 end
